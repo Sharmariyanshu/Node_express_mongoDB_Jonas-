8079c68286bb654fb6d5cf0c87cbe1a4911d5fb1
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-13 21:07:36 +0530

    Routing

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
-
 const http = require('http');
+const url = require('url');
 
 // SERVER
 const server = http.createServer((req, res) => {
-    console.log(req);
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is PRODUCT');
+    }else{
+        res.writeHead(404, {
+            'content-type':
+            'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
