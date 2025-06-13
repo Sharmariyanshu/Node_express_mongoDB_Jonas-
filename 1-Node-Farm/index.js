b9b9f3b316603402dc0b426923a06c7ceaf36239
Riyanshu Sharma <riyanshu062@gmail.com>
2025-06-14 00:27:51 +0530

    Simple API

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -2,7 +2,13 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+//--------------------------------------------------------------------------------------------------------------//
+
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj= JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
@@ -10,10 +16,12 @@ const server = http.createServer((req, res) => {
         res.end('This is OVERVIEW');
     } else if (pathName === '/product') {
         res.end('This is PRODUCT');
-    }else{
+    } else if (pathName === '/api') {
+         res.writeHead(200, { 'Content-type': 'application/json'});
+            res.end(data);
+        }else{
         res.writeHead(404, {
-            'content-type':
-            'text/html',
+            'content-type':'text/html',
             'my-own-header': 'hello-world'
         });
         res.end('<h1>Page not found!</h1>');
